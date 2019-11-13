abaa922bd9e023b3e005e869f4ce03534cdaa83e
Sarah Matevia <kittyazul@yahoo.com>
2019-11-13 17:26:09 -0500

    Add files via upload

---
+++ Senior_Design/environment/EVERYTHING/app/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+app = Flask(__name__)
+
+from app import hello
---
+++ Senior_Design/environment/EVERYTHING/app/hello.py
@@ -0,0 +1,7 @@
+from app import app
+from flask import request
+from flask import render_template
+
+@app.route('/')
+def main():
+    return render_template("home.html")
