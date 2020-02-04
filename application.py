ee566583cb182da855f2cfe446eebca38edc0185
Matevia <matevisl@ucmail.uc.edu>
2020-02-03 23:46:04 -0500

    added the render template framework

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # print a nice greeting.
 def say_hello(username = "World"):
@@ -8,7 +8,7 @@ def say_hello(username = "World"):
 application = Flask(__name__)
 
 # some bits of text for the page.
-@application.route('/app')
+@application.route('/')
 def main():
     return render_template("Test.html")
 
