d80902a57dafdbfef13ee5fa7c9bf33770c3efad
Matevia <matevisl@ucmail.uc.edu>
2020-02-04 00:00:24 -0500

    revert

--- application.py
+++ application.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
 
 # print a nice greeting.
 def say_hello(username = "World"):
     return '<p>Hello %s!</p>\n' % username
-
 
 # some bits of text for the page.
 header_text = '''
@@ -14,12 +13,22 @@ instructions = '''
     someone specific.</p>\n'''
 home_link = '<p><a href="/">Back</a></p>\n'
 footer_text = '</body>\n</html>'
+
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
+
+# add a rule for the index page.
+application.add_url_rule('/', 'index', (lambda: header_text +
+    say_hello() + instructions + footer_text))
+
+# add a rule when the page is accessed with a name appended to the site
+# URL.
+application.add_url_rule('/<username>', 'hello', (lambda username:
+    header_text + say_hello(username) + home_link + footer_text))
 
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    application.debug = False
+    application.debug = True
     application.run()
