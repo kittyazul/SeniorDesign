5573fd81b97b35fe8dc0132e1f2d4ad273f9f41b
Matevia <matevisl@ucmail.uc.edu>
2020-02-03 23:30:02 -0500

    Breaking things with less code

--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask import request
-from flask import render_template
-
-app = Flask(__name__)
-
-from app import hello
--- app/hello.py
+++
@@ -1,7 +0,0 @@
-from app import app
-from flask import request
-from flask import render_template
-
-@app.route('/')
-def main():
-    return render_template("Test.html")
