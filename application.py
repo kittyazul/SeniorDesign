0b2f02bd94e143db236e911023035163f57074e9
Matevia <matevisl@ucmail.uc.edu>
2020-02-10 15:54:52 -0500

    Edited the front-end

--- application.py
+++ application.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route('/')
-def display():
-    return render_template('Index.html')
+@application.route('/<page_name>')
+def display(page_name=None):
+    if page_name is None:
+        return render_template('Index.html')
+    else:
+        return render_template(page_name)
 
