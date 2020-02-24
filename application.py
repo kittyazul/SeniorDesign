716dd77cf6a0948b697cfe2fbf4214b23107ab5d
Katie <katiedrury123@yahoo.com>
2020-02-24 14:07:48 -0500

    added code

--- application.py
+++ application.py
@@ -1,6 +1,26 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
+import requests
+import json
 application = Flask(__name__)
+
+
+@application.route('/scan', methods=['GET', 'POST'])
+def scan(ScanURL=None):
+    if request.method == 'GET' and not ScanURL:
+        return render_template('D-Phish.html')
+    if request.method =='POST':
+        if 'ScanURL' in request.form:
+            ScanURL = request.form['ScanURL']
+        if ScanURL == '':
+            return render_template('D-Phish.html')
+    else:
+        return render_template('D-Phish.html')		
+    url = 'https://www.virustotal.com/vtapi/v2/url/scan'
+    params = {'apikey': '989c64be41dd704e7e834beb72774fe3e4c837e15bd1807f2284eb563e62135b', 'url':ScanURL}
+    response = requests.post(url, data=params)
+    return redirect(response.json()['permalink'])
+    #print(json.dumps(response.json(), indent=4, sort_keys=True))
+
 
 @application.route('/')
 @application.route('/<page_name>')
---
+++ create_zip.sh
@@ -0,0 +1,2 @@
+rm seniordesign.zip
+python -m zipfile -c seniordesign.zip application.py requirements.txt static templates
