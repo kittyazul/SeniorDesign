6e8f88ce6472037b9eb49c0f738d60f8b90a6735
Matevia <matevisl@ucmail.uc.edu>
2020-03-26 18:08:00 -0400

    Testing SSO merger with Katie files

---
+++ Serge/Quizzes/JavaScript/History-Quiz.js
@@ -0,0 +1,187 @@
+const options=document.querySelector(".options").children;
+const answerTrackerContainer=document.querySelector(".answers-tracker");
+const questionNumberSpan=document.querySelector(".question-num-value");
+const totalQuestionSpan=document.querySelector(".total-question");
+const correctAnswerSpan=document.querySelector(".correct-answers");
+const totalQuestionSpan2=document.querySelector(".total-question2");
+const percentage=document.querySelector(".percentage");
+const question=document.querySelector(".question");
+const op1=document.querySelector(".option1");
+const op2=document.querySelector(".option2");
+const op3=document.querySelector(".option3");
+const op4=document.querySelector(".option4");
+let questionIndex;
+let index=0;
+let myArray=[];
+let myArr=[]; //For testing quizIndex value
+let score=0;
+
+// Questions, Options, and Answers
+const questions=[
+    {
+        q: 'Who was the first group of hackers that banded together to conduct the first phishing scam recorded in 1990, and ultimately history?',
+        options: [
+            'Anonymous',
+            'The Warez Community',
+            'Legion of Doom', 
+            'Masters of Deception'],
+        answer:1
+    },
+    {
+        q: 'Phishing attacks have evolved over the years. As of 2019, what type of legitimate services did phishing attacks begin targeting?',
+        options: [
+            'Health Services',
+            'Bank Account Services',
+            'Cloud Services', 
+            'Software Services'],
+        answer:2
+    },
+    {
+        q: 'What is the first thing phishing attempts expect out of you?',
+        options: [
+            'A fast and irrational response based on emotions',
+            'A calm and composed response',
+            'Nothing, they expect you to ignore them', 
+            'None of the above'],
+        answer:0
+    },
+    {
+        q: 'What should you do when you are pressured online (or via email) into doing something without hesitation?',
+        options: [
+            'Do the exact opposite of what is being asked',
+            'Calmly double check the information',
+            'Consult an expert/trusted outside source', 
+            'All of the Above'],
+        answer:3
+    },
+    {
+        q: 'What is the biggest cyber-attack in History?',
+        options: [
+            'The Playstation hack (2011)',
+            'The Sony hack (2014)',
+            'America Online (AOL, 1990)', 
+            'None of the above'],
+        answer:2
+    },
+]
+
+//Set questions and options and question number
+
+totalQuestionSpan.innerHTML=questions.length;
+function load(){
+    questionNumberSpan.innerHTML=index+1;
+    question.innerHTML=questions[questionIndex].q;
+    op1.innerHTML=questions[questionIndex].options[0];
+    op2.innerHTML=questions[questionIndex].options[1];
+    op3.innerHTML=questions[questionIndex].options[2];
+    op4.innerHTML=questions[questionIndex].options[3];
+    index++;
+}
+
+function check(element){
+    if(element.id==questions[questionIndex].answer){
+        element.classList.add("correct");
+        updateAnswerTracker("correct")
+        score++;
+        //console.log("score: "+score)
+    }
+    else{
+        element.classList.add("wrong");
+        updateAnswerTracker("wrong")
+    }
+
+    disabledOptions()
+}
+
+function disabledOptions(){
+    for(let i=0; i<options.length; i++) {
+        options[i].classList.add("disabled");
+        if(options[i].id==questions[questionIndex].answer) {
+            options[i].classList.add("correct");
+        }
+    }
+}
+
+function enableOptions(){
+    for(let i=0; i<options.length; i++) {
+        options[i].classList.remove("disabled", "correct", "wrong");
+    }
+}
+
+function validate(){
+    if(!options[0].classList.contains("disabled")){
+        alert("Please select one option before moving on.")
+    }
+    
+    else{
+        enableOptions();
+        randomQuestion();
+    }
+}
+
+function next(){
+    validate();
+}
+
+function randomQuestion(){
+    let randomNumber=Math.floor(Math.random()*questions.length);
+    let hitDuplicate=0;
+        if (index==questions.length){
+            //console.log("Quiz Over!")
+            quizOver();
+        }
+        else{
+            if (myArray.length>0){
+                for (let i=0; i<myArray.length; i++) {
+                    if(myArray[i]==randomNumber){
+                        hitDuplicate=1;
+                        break;
+                    }
+                }
+                if (hitDuplicate==1){
+                    randomQuestion();
+                }
+                else {
+                    questionIndex=randomNumber;
+                    load();
+                    myArr.push(questionIndex);
+                }
+
+            }
+
+            if (myArray.length==0){
+                questionIndex=randomNumber;
+                load();
+            }
+
+            //console.log("myArr:"+ myArr); //Testing
+            myArray.push(randomNumber);
+        }
+ }
+ 
+function answerTracker(){
+     for(let i=0; i<questions.length; i++){
+         const div=document.createElement("div")
+         answerTrackerContainer.appendChild(div);
+     }
+}
+
+function updateAnswerTracker(classNam){
+    answerTrackerContainer.children[index-1].classList.add(classNam); 
+}
+
+function quizOver(){
+    document.querySelector(".quiz-over").classList.add("show");
+    correctAnswerSpan.innerHTML=score;
+    totalQuestionSpan2.innerHTML=questions.length;
+    percentage.innerHTML=(score/questions.length)*100 + "%";
+}
+
+function tryAgain(){
+    window.location.reload();
+}
+
+window.onload=function(){
+    randomQuestion();
+    answerTracker();
+}
--- create_zip.sh
+++ create_zip.sh
@@ -1,2 +1,2 @@
 rm seniordesign.zip
-python -m zipfile -c seniordesign.zip application.py requirements.txt static templates
+python -m zipfile -c seniordesign.zip application.py requirements.txt static templates .ebextensions saml
