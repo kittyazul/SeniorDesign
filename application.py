406cdb1341b59751b2bff867aaa8984e5a2c8301
Matevia <matevisl@ucmail.uc.edu>
2020-03-27 18:09:10 -0400

    Seperate mailto and input

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import requests
 import json
+import os
 application = Flask(__name__)
 
 
@@ -9,19 +10,19 @@ def scan(ScanURL=None):
     if request.method == 'GET' and not ScanURL:
         return render_template('D-Phish.html')
     if request.method =='POST':
-        if 'ScanURL' in request.form:
+        if 'CheckURL' in request.form:
             ScanURL = request.form['ScanURL']
         if ScanURL == '':
             return render_template('D-Phish.html')
     else:
         return render_template('D-Phish.html')		
     url = 'https://www.virustotal.com/vtapi/v2/url/scan'
-    params = {'apikey': 'VirusTotalApi', 'url':ScanURL}
+    params = {'apikey': os.getenv('VirusTotalApi'), 'url':ScanURL}
     response = requests.post(url, data=params)
     return redirect(response.json()['permalink'])
     #print(json.dumps(response.json(), indent=4, sort_keys=True))
 
-
+@application.route('/')
 @application.route('/<page_name>')
 def display(page_name=None):
     if page_name is None:
